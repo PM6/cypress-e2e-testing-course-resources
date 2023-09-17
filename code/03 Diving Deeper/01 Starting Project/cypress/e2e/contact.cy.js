fb04aa23c48d2458a131e551c440e046e7413223
patri <patrickmarkgraf@gmail.com>
2023-09-17 09:05:58 +0200

    add test using then

--- code/03 Diving Deeper/01 Starting Project/cypress/e2e/contact.cy.js
+++ code/03 Diving Deeper/01 Starting Project/cypress/e2e/contact.cy.js
@@ -16,4 +16,13 @@ describe('contact form', () => {
             .contains('Sending...')
             .should('have.attr', 'disabled');
     });
+
+    it('should validate the form input', () => {
+        cy.visit('http://localhost:5173/about');
+        cy.get('[data-cy="contact-btn-submit"').click();
+        cy.get('[data-cy="contact-btn-submit"').then(el => {
+            expect(el).to.not.have.attr('disabled');
+            expect(el.text()).to.not.equal('Sending...');
+        });
+    });
 });
